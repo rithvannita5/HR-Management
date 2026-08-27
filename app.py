6dbfc396b8450e78579dd8141fea497d8fec1ee3
rithvannita5 <rithvannita5@gmail.com>
2026-08-27 07:54:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6715,6 +6715,9 @@ REPORT_HTML = '''<!DOCTYPE html>
 # MAIN
 # ============================================================
 
+import os
+
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
